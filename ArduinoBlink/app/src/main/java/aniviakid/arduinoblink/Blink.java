0eefd3c26def8ec2fd0300b562f1175926c2837a
张子豪 <31848851+AniviaKid@users.noreply.github.com>
2019-02-16 16:49:10 +0800

    update

--- ArduinoBlink/app/src/main/java/aniviakid/arduinoblink/Blink.java
+++ ArduinoBlink/app/src/main/java/aniviakid/arduinoblink/Blink.java
@@ -16,6 +16,11 @@ public class Blink extends UsbArduino {
         super(appContext);
     }
 
+    int thread = 0; //占空比
+    int period = 500; //周期
+    int i = 0;
+    int flag = 1; //flag为1由暗至亮，flag为-1由亮至暗
+
     @Override
     protected void setup() {
 // initialize digital pin LED_BUILTIN as an output.
@@ -24,9 +29,28 @@ public class Blink extends UsbArduino {
 
     @Override
     protected void loop() {
-        digitalWrite(DigitalPin.PIN_13, DigitalState.HIGH);   // turn the LED on (HIGH is the voltage level)
+       /* digitalWrite(DigitalPin.PIN_13, DigitalState.HIGH);   // turn the LED on (HIGH is the voltage level)
         delay(1000);                       // wait for a second
         digitalWrite(DigitalPin.PIN_13,DigitalState.LOW);    // turn the LED off by making the voltage LOW
-        delay(1000);
+        delay(1000);*/
+        if (i < thread) { //高电平
+            digitalWrite(DigitalPin.PIN_13, DigitalState.HIGH);
+            i++;
+        }
+        else if (i >= thread && i < period) { //低电平
+            digitalWrite(DigitalPin.PIN_13, DigitalState.LOW);
+            i++;
+        }
+        else if (i == period) { //一个周期执行完改变占空比
+            i = 0;
+            if (flag == 1) {
+                thread++;
+                if (thread == period) flag *= -1;
+            }
+            else {
+                thread--;
+                if (thread == 0) flag *= -1;
+            }
+        }
     }
 }
